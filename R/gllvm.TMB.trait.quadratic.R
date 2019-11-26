b7e1c15c87a113d26490fddd0a772754cf5c416f
Bert van der Veen <bert.vanderveen@nibio.no>
2019-11-26 09:31:19 +0100

    Tweak scaling of optimization

--- R/gllvm.TMB.trait.quadratic.R
+++ R/gllvm.TMB.trait.quadratic.R
@@ -421,10 +421,10 @@ gllvm.TMB.trait.quadratic <- function(y, X = NULL, TR = NULL, formula = NULL, nu
             timeo <- system.time(optr <- try(nlminb(objr$par, objr$fn, objr$gr, control = list(rel.tol = reltol, maxit = maxit, trace = trace2)), 
                 silent = !trace2))
         }
-        if (optimizer == "optim") {
+        if (optimizer == "optim" & starting.val!="zero") {
           if(!is.null(par.scale)){
             if(par.scale=="coef"){
-              parscale<-objr$par
+              parscale<-abs(objr$par)
               parscale[parscale==0]<-1
             }else if(is.numeric(par.scale)){
               parscale<-rep(par.scale,length(objr$par))
