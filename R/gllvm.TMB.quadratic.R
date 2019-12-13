33f5c68782be69dc1cb84b8b4dfb57c1b693a9ee
Bert van der Veen <bertv@bertv-laptop.math.ntnu.no>
2019-12-13 12:04:01 +0100

    Like this, users can specify their own parallel backend or openmp, outside the model call to parallelize.

--- R/gllvm.TMB.quadratic.R
+++ R/gllvm.TMB.quadratic.R
@@ -501,7 +501,7 @@
               }
                 if(n.init>1){
                   #start.values.gllvm.TMB.quadratic<-getFromNamespace("start.values.gllvm.TMB.quadratic","gllvm.quadratic")
-                  results<-foreach(i=1:n.init,.errorhandling = "pass",.export=ls(), .combine='list', .packages="gllvm",.verbose=FALSE) %dopar% {
+                  results<-foreach(i=1:n.init,.errorhandling = "pass",.export=ls(), .multicombine=T, .inorder=F, .packages="gllvm",.verbose=FALSE) %dopar% {
                     madeMod<-makeMod(i)
                     #found the issue, was exporting packages. Now I need to find out how to set a seed inside a foreach..might have to set outside the function inside the forach loop due to openmp
                     return(madeMod)
